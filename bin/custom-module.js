954aa11a0ca30ac83540b0b37c83454c7ab7bfca
JohnNojwo <59100485+JohnNojwo@users.noreply.github.com>
2020-07-30 17:31:19 +0200

    Add files via upload

---
+++ bin/custom-module.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const path = require('path');
+
+const [, , ...args] = process.argv;
+
+const command = args[0];
+
+switch (command) {
+    /*case 'build':
+        require('../scripts/build');
+        break;
+
+    case 'deploy':
+        require('../scripts/deploy');
+        break;
+
+    case 'init':
+        require('../scripts/init');
+        break;
+
+    case 'init-ts':
+        require('../scripts/init-ts');
+        break;*/
+        case 'test':
+        require('../scripts/test');
+        break;
+}
